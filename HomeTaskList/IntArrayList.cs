9d807bb1f1957e7ba891213db2713eedae0ee864
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-18 02:46:43 +0400

    Add files via upload

---
+++ HomeTaskList/IntArrayList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskList
+{
+    internal class IntArrayList
+    {
+        int[] _arr;
+        public int Count { get; private set; }
+        public int Capacity { get; set; }
+        public IntArrayList(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new int[Capacity];
+        }
+        public IntArrayList()
+        {
+            Capacity = 5;
+            _arr = new int[Capacity];
+        }
+        public int this[int num]
+        {
+            get { return _arr[Count]; }
+            set { _arr[Count] = value; }
+        }
+        public void Add(int a)
+        {
+            if (_arr.Length == Count)
+            {
+                Array.Resize(ref _arr, Capacity + Count);
+            }
+            _arr[Count] = a;
+            Count++;
+        }
+        public void IndexOf(int value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_arr[i] == value)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+        public void GetInfo()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+        public override string ToString()
+        {
+              string a = "{";
+            for (int i = 0; i < Count; i++)
+            {
+                if (i > 0)
+                {
+                    a =a + " , ";
+                }
+                a += _arr[i].ToString();
+            }
+            a += "}";
+            return a;
+        }
+    }
+}
+
---
+++ HomeTaskList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeTaskList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          IntArrayList list = new IntArrayList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
